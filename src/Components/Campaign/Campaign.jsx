09c5e770293cd9fc5618c70faafbe5e54e01c857
proazad <proazadhossain7595@gmail.com>
2023-09-24 22:25:14 +0300

    Home All Loading Complete and show all data

--- src/Components/Campaign/Campaign.jsx
+++ src/Components/Campaign/Campaign.jsx
@@ -1,9 +1,37 @@
-const Campaign = () => {
+import PropTypes from "prop-types";
+const Campaign = ({ campaign }) => {
+  const {
+    id,
+    picture,
+    title,
+    category,
+    card_bg_color,
+    button_bg_color,
+    text_color,
+  } = campaign;
+  const handleCampaignDetails = (id) => {
+    console.log(id);
+  };
   return (
-    <div className="">
-      <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
+    <div
+      onClick={() => handleCampaignDetails(id)}
+      className={`rounded-md space-y-4 bg-[${card_bg_color}]`}
+    >
+      <img src={picture} alt="" draggable="false"></img>
+      <div className="p-5 space-y-5">
+        <p
+          className={`inline py-2 px-3 rounded-md text-[${text_color}] bg-[${button_bg_color}] text-sm font-medium`}
+        >
+          {category}
+        </p>
+        <h2 className={`text-lg font-semibold text-[${text_color}]`}>
+          {title}
+        </h2>
+      </div>
     </div>
   );
 };
-
+Campaign.propTypes = {
+  campaign: PropTypes.object,
+};
 export default Campaign;
--- src/Components/Campaigns/Campaigns.jsx
+++ src/Components/Campaigns/Campaigns.jsx
@@ -1,151 +1,19 @@
+import { useEffect, useState } from "react";
+import Campaign from "../Campaign/Campaign";
+
 const Campaigns = () => {
+  const [campaigns, setCampaigns] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setCampaigns(data));
+  }, []);
   return (
     <>
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        {/* Card 1  */}
-        <div className="bg-[#0052ff26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#0052FF] bg-[#0052ff33] text-sm font-medium">
-              Health
-            </p>
-            <h2 className="text-lg font-semibold text-[#0052FF]">
-              Clean water for children
-            </h2>
-          </div>
-        </div>
-        {/* Card 2  */}
-        <div className="bg-[#ff444a26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#FF444A] bg-[#ff444a33] text-sm font-medium">
-              Education
-            </p>
-            <h2 className="text-lg font-semibold text-[#FF444A]">
-            Good education
-            </h2>
-          </div>
-        </div>
-        {/* Card 3  */}
-        <div className="bg-[#79c23f26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#FF444A] bg-[#79c23f33] text-sm font-medium">
-              Education
-            </p>
-            <h2 className="text-lg font-semibold text-[#79C23F]">
-            Good education
-            </h2>
-          </div>
-        </div>
-         {/* Card 4  */}
-         <div className="bg-[#0052ff26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#0052FF] bg-[#0052ff33] text-sm font-medium">
-              Health
-            </p>
-            <h2 className="text-lg font-semibold text-[#0052FF]">
-              Clean water for children
-            </h2>
-          </div>
-        </div>
-        {/* Card 1  */}
-        <div className="bg-[#0052ff26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#0052FF] bg-[#0052ff33] text-sm font-medium">
-              Health
-            </p>
-            <h2 className="text-lg font-semibold text-[#0052FF]">
-              Clean water for children
-            </h2>
-          </div>
-        </div>
-        {/* Card 2  */}
-        <div className="bg-[#ff444a26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#FF444A] bg-[#ff444a33] text-sm font-medium">
-              Education
-            </p>
-            <h2 className="text-lg font-semibold text-[#FF444A]">
-            Good education
-            </h2>
-          </div>
-        </div>
-        {/* Card 3  */}
-        <div className="bg-[#79c23f26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#FF444A] bg-[#79c23f33] text-sm font-medium">
-              Education
-            </p>
-            <h2 className="text-lg font-semibold text-[#79C23F]">
-            Good education
-            </h2>
-          </div>
-        </div>
-         {/* Card 4  */}
-         <div className="bg-[#0052ff26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#0052FF] bg-[#0052ff33] text-sm font-medium">
-              Health
-            </p>
-            <h2 className="text-lg font-semibold text-[#0052FF]">
-              Clean water for children
-            </h2>
-          </div>
-        </div>
-        {/* Card 1  */}
-        <div className="bg-[#0052ff26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#0052FF] bg-[#0052ff33] text-sm font-medium">
-              Health
-            </p>
-            <h2 className="text-lg font-semibold text-[#0052FF]">
-              Clean water for children
-            </h2>
-          </div>
-        </div>
-        {/* Card 2  */}
-        <div className="bg-[#ff444a26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#FF444A] bg-[#ff444a33] text-sm font-medium">
-              Education
-            </p>
-            <h2 className="text-lg font-semibold text-[#FF444A]">
-            Good education
-            </h2>
-          </div>
-        </div>
-        {/* Card 3  */}
-        <div className="bg-[#79c23f26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#FF444A] bg-[#79c23f33] text-sm font-medium">
-              Education
-            </p>
-            <h2 className="text-lg font-semibold text-[#79C23F]">
-            Good education
-            </h2>
-          </div>
-        </div>
-         {/* Card 4  */}
-         <div className="bg-[#0052ff26] rounded-md space-y-4">
-          <img src="https://i.ibb.co/HNybchr/Education.png" alt="" />
-          <div className="p-5 space-y-5">
-            <p className="inline py-2 px-3 rounded-md text-[#0052FF] bg-[#0052ff33] text-sm font-medium">
-              Health
-            </p>
-            <h2 className="text-lg font-semibold text-[#0052FF]">
-              Clean water for children
-            </h2>
-          </div>
-        </div>
+        {campaigns.map((campaign) => (
+          <Campaign key={campaign.id} campaign={campaign}></Campaign>
+        ))}
       </div>
     </>
   );
