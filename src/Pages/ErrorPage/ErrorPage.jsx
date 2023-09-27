3933846895a3d91e0de7a66b12f064730f63e345
proazad <proazadhossain7595@gmail.com>
2023-09-27 20:05:29 +0300

    Update error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
     <div className="container mx-auto">
@@ -5,6 +7,9 @@ const ErrorPage = () => {
         <h2 className="text-5xl">Oops!!</h2>
         <h3 className="text-4xl font-semibold">404</h3>
         <p className="text-3xl">Not Found</p>
+        <p className="my-10">
+          <NavLink to="/" className="btn btn-sm btn-secondary">Back to Home</NavLink>
+        </p>
       </div>
     </div>
   );
